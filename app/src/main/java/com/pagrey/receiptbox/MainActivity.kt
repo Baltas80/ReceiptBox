d6c774f8e15b658cd858ac9372160bf16a81dff1
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-13 09:31:25 +0200

    Enable explicit edge-to-edge system UI support

--- app/src/main/java/com/pagrey/receiptbox/MainActivity.kt
+++ app/src/main/java/com/pagrey/receiptbox/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pagrey.receiptbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -18,6 +19,7 @@ import com.pagrey.receiptbox.ui.theme.ReceiptBoxTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { ReceiptBoxApp() }
     }
 }
